adf1fd804d5b350c6eecaa75bdcbafa6bb2c46b1
Angelthewinner <angelwynarciaga@gmail.com>
2026-08-12 07:54:32 -0400

    Update Tracker.js

--- Tracker.js
+++ Tracker.js
@@ -3,14 +3,26 @@ let username;
 const searchbutton = document.getElementById("search").onclick = async function(){
     username = document.getElementById("username-input").value;
     try {
-        const response = await fetch(`http://127.0.0.1:8001/githubevents?username=${username}`);
+        const response = await fetch(`https://api.github.com/users/${username}/events`);
         const data = await response.json();
         const result = document.getElementById('box');
-        let htmlstring = '';
+        const events = [];
         for(const i in data){
-            htmlstring += data[i] + "<br>"
-        }
-        result.innerHTML = htmlstring;
+            events.push(
+                "-User: " + data[i].actor.login,
+                "Event: " + data[i].type,
+                "Repo: " + data[i].repo.name,
+                "Date: " + data[i].created_at,
+                "Public: " + data[i].public,
+                ""
+            )
+            }
+        let htmlstring = '';
+            for(const i in events){
+                htmlstring += events[i] + "<br>"
+            }
+    console.log(events)
+    result.innerHTML = htmlstring;
     } catch (error) {
         console.error('Error:', error);
     }
@@ -21,19 +33,28 @@ const buttondown = document.addEventListener("keydown", async event => {
     username = document.getElementById("username-input").value;
      if(event.key.startsWith("Enter")){
         try {
-            const response = await fetch(`http://127.0.0.1:8001/githubevents?username=${username}`);
-            const data = await response.json();
-            const result = document.getElementById('box');
-            let htmlstring = '';
-            for(const i in data){
-                htmlstring += data[i] + "<br>"
+        const response = await fetch(`https://api.github.com/users/${username}/events`);
+        const data = await response.json();
+        const result = document.getElementById('box');
+        const events = [];
+        for(const i in data){
+            events.push(
+                "-User: " + data[i].actor.login,
+                "Event: " + data[i].type,
+                "Repo: " + data[i].repo.name,
+                "Date: " + data[i].created_at,
+                "Public: " + data[i].public,
+                ""
+            )
             }
-        result.innerHTML = htmlstring
+        let htmlstring = '';
+            for(const i in events){
+                htmlstring += events[i] + "<br>"
+            }
+    console.log(events)
+    result.innerHTML = htmlstring;
     } catch (error) {
         console.error('Error:', error);
     }
     };
 });
-
-
-
